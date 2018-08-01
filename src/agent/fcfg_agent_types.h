77df24761ebbf5ce154f46b20905f8412ce5b85a
xuzhengqiang <zhengqiang.xu@yundongpai.net>
2018-08-01 09:04:56 +0800

    add fcfg_agent_types.h

---
+++ src/agent/fcfg_agent_types.h
@@ -0,0 +1,23 @@
+#ifndef _FCFG_AGENT_TYPES_H
+#define _FCFG_AGENT_TYPES_H
+
+#include "fastcommon/common_define.h"
+
+typedef struct tagFCFGJoinResp {
+    int64_t center_cfg_version;
+} FCFGJoinResp;
+
+typedef struct tagFCFGPushConfigHeader {
+    short count;  //config count in body
+} FCFGPushConfigHeader;
+
+typedef struct tagFCFGPushConfigBodyPart {
+    unsigned char status;
+    unsigned char name_len;
+    int value_len;
+    int64_t version;
+    int create_time;
+    int update_time;
+} FCFGPushConfigBodyPart;
+
+#endif
